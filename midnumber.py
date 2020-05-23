13a1968dcae9e346a74d8a0afcbd76d87d759f72
Ritik Kumar <ritikkumar262@gmail.com>
2020-05-23 18:12:50 +0530

    Sorted the codde

--- midnumber.py
+++ midnumber.py
@@ -1,30 +1,13 @@
-i = int(input('Enter times: '))
-list = []
+# python
+def MidNumber(arr) :
+    ans = 0
+    if len(arr)%2!=0 :
+        ans = arr[((len(arr)+1)/2)-1]
+    else :
+        ans = arr[((len(arr))/2)-1]
+    return ans
 
-def MidNumber() :
-    for a in range(0, i) :
-        a = int(input('Enter the count: '))
-        for b in range(0, a):
-            b = int(input('Enter list elements: '))
-            list.append(b)
-        
-        print(list)
-        n = len(list)
-        if (n % 2 == 0):
-            m = n / 2 + 1
-            print(list[int((n/2))])
-        else :
-             print(list[int((n+1/2)-1)])
-        list.clear()
-
-MidNumber()
-print('Want to repeat??')
-print('Yes OR No')
-ans = input('Enter Response: ')
-
-if ans == 'yes' :
-    MidNumber()
-elif ans == 'no' :
-    print('OK Thanks')
-else :
-    print('Sorry!! I don\'t understand')
+test_cases = int(input('Enter test cases: '))
+for i in range(test_cases) :
+    list1 = list(map(int, input('Enter list element seperated by spaces: ')))
+    print(MidNumber(list1))
